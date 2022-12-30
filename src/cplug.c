e95e7dd274b8745a07e1f6874ca5e7a2fad0a7ff
Kravantokh <mamaia7777@gmail.com>
2022-12-30 18:38:59 +0200

    Finally working.

--- src/cplug.c
+++ src/cplug.c
@@ -2,36 +2,49 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <math.h>
+#include "base"
+
+void* alloc(unsigned int n){
+	return malloc(n);
+}
+
+void print(plug_api_string str){
+	printf("%s", str.contents);
+}
 
 int main(void){
 	
-	char msg[256] = {0};
-	int ret = system("./tcc/tcc -L ./tcc/ -I ./tcc/ ./tcc/test.c -o tcc/a.out");
-
-	printf("Return value of tcc: %d\n", ret);
-	FILE* buf = popen("./tcc/a.out", "r");
+	int ret = system("./tcc/tcc -L ./tcc/ -I ./tcc/ -shared ./tcc/test.c -o tcc/test.so");
 	
-	if( fgets (msg, 256, buf)!=NULL ) {
-      puts(msg);
-	}
-	ret = pclose(buf);
-	printf("Return value a.out: %d\n", ret);
-
-
-	void* handle = dlopen("./test.so", RTLD_LAZY | RTLD_LOCAL);
-	if(handle == NULL){
-		fprintf(stderr, "Handle is NULL.\n");
-	}
-
-	double (*test)(void) = dlsym(handle, "test");
-	if(test == NULL){
-		fprintf(stderr, "test fptr is NULL.\n");
+	if(ret != 0){
+		fprintf(stderr, "TCC did not compile sucessfully!\n");
 		return EXIT_FAILURE;
 	}
 
-	printf("Test result: %f", test() );
-	
-	dlclose(handle);
 
-	return EXIT_SUCCESS;
+	void* handle = dlopen("./tcc/test.so", RTLD_LAZY | RTLD_LOCAL);
+	if(handle == NULL){
+		fprintf(stderr, "Handle is NULL. Could not dlopen the file given.\n");
+		return EXIT_FAILURE;
+	}
+
+	void (*pre_init)(plug_api_dispatch_table) = dlsym(handle, "pre_init");
+
+	if(pre_init == NULL){
+		fprintf(stderr, "pre_init function ptr is NULL. The init function could not be found. \n");
+		return EXIT_FAILURE;
+	}
+
+	pre_init( (plug_api_dispatch_table){&alloc, &print}) ;
+
+
+	void (*plug_main)(void) = dlsym(handle, "main");
+
+	if(plug_main == NULL){
+		fprintf(stderr, "plug_main function ptr is NULL. The main function could not be found. \n");
+		return EXIT_FAILURE; }
+	plug_main();
+	dlclose(handle); 
+
+return EXIT_SUCCESS;
 }
